dd46a16535ee105867adcf468a403692ef2b9bb1
Aleksander Püüa <aleksander.puua@voco.ee>
2024-12-02 09:27:41 +0200

    Kasutaja loomine Notepad .csv faili abil PS-s

---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,18 @@
+﻿# määrame, kus asub fail, mille sees on kasutajate andmed
+$Fail = "C:\Users\Aleksander\kasutajate_haldus\kasutajad.csv"
+# loeme failist sisu, info jaotatakse ; järgi
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+# failis iga kasutaja andmestik on eraldi reas
+# vaatame faili siu ridade kaupa
+# selleks salvestame iga rea $kasutaja muutuja sisse
+foreach ($kasutaja in $Kasutajad)
+{
+    # kuna reas on mitu väärtust, siis iga element on kättesaadav veeru nimetuse järgi, milleks me paneme
+    # $muutuja.VeeruNimetus kujul kirja
+    $Kasutajanimi = $kasutaja.Kasutajanimi
+    $Taisnimi = $kasutaja.Taisnimi
+    $KontoKirjeldus =$kasutaja.Kontokirjeldus
+    $Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+    # kasutades saadud info lisame kasutaja
+    New-LocalUser -Name $Kasutajanimi -Password $Parool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+}
