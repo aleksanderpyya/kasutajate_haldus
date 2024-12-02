8d1b6a9e9e432f6c613cf7cac8d8c3f9046c2a3f
Aleksander Püüa <aleksander.puua@voco.ee>
2024-12-02 09:42:38 +0200

    Lokaalse kasutaja loomine tema ees- ja perenime järgi

---
+++ kasutaja_loomine_3.ps1
@@ -0,0 +1,18 @@
+﻿$firstname = Read-Host 'Sisesta oma eesnimi'
+$lastname = Read-Host 'Sisesta oma perenimi'
+
+$username = $firstname.ToLower() + "." + $lastname.ToLower()
+$userPassword = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+$fullname = $firstname + " " + $lastname
+$description = "Local account"
+
+Write-Host 'Loodud kasutaja on ' $username
+
+$userExists = Get-LocalUser $username -ErrorAction SilentlyContinue
+
+if(($userExists | Select Name).Name.Count -ge 1){
+    Write-Host 'Problem to create user - user already exists'
+    Exit
+} else {
+    New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
+}
